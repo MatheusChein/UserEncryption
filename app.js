e309f6bd2fb74e33b852e923015e65d380eb264f
Matheus Chein <matheuschein@gmail.com>
2020-05-27 15:06:17 -0300

    Adding hashed passwords (which is the real level 3 security)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 mongoose.connect('mongodb://localhost/userDB', {useUnifiedTopology: true, useNewUrlParser: true});
 
@@ -17,8 +17,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); 
 
 const User = mongoose.model("User", userSchema);
 
@@ -37,7 +35,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   let newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err) {
@@ -51,7 +49,7 @@ app.post("/register", function(req, res){
 app.post("/login", function(req, res){
   User.findOne({email: req.body.username}, function(err, foundUser){
     if (foundUser) {
-      if (foundUser.password === req.body.password) {
+      if (foundUser.password === md5(req.body.password)) {
         res.render("secrets");
       } else {
         res.redirect("/login");

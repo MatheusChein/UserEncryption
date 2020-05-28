87214a4c08adff84a9da0e6500e72c5c48302d02
Matheus Chein <matheuschein@gmail.com>
2020-05-28 16:21:11 -0300

    App finalized with the option to submit secrets working

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
-  facebookId: String
+  facebookId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -111,9 +112,21 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-  console.log(req.isAuthenticated());
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        console.log(foundUsers);
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
@@ -153,7 +166,22 @@ app.post("/login", function(req, res){
   });
 });
 
-
+app.post("/submit", function(req, res){
+  let submittedSecret = req.body.secret;
+  console.log(req.user.id);
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
 
 
 
